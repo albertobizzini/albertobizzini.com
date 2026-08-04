9e99391b151324e6aaa5271a9d8d67348b714f32
Alberto Bizzini <albertobizzini@hotmail.com>
2026-08-04 18:02:44 +0200

    Add Substitution table and update vw_ExportableClipping
    Added [dbo].[Substitution] table for title overrides. Updated vw_ExportableClipping to union Clipping/OldClipping via CTE and join Substitution for NewTitle. Included Substitution.sql in project and updated bacpac for schema changes.

---
+++ KindleClippings.Database/dbo/Tables/Substitution.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Substitution] (
+    [ClippingId] NVARCHAR (140)  NOT NULL,
+    [NewTitle]   NVARCHAR (1000) NOT NULL,
+    CONSTRAINT [PK_Substitution] PRIMARY KEY CLUSTERED ([ClippingId] ASC)
+);
+
--- KindleClippings.Database/dbo/Views/vw_ExportableClipping.sql
+++ KindleClippings.Database/dbo/Views/vw_ExportableClipping.sql
@@ -1,33 +1,53 @@
 ﻿CREATE VIEW [dbo].[vw_ExportableClipping] AS
-SELECT 
-	c.Id,
-	c.Title,
-	c.Author,
-	c.Type,
-	c.Page,
-	c.StartLocation,
-	c.EndLocation,
-	c.AddedOn,
-	c.Text,
-	c.ImportedAt
-FROM
-	dbo.Clipping AS c
-WHERE
-	c.ID NOT IN (SELECT Id FROM dbo.IgnorableClipping)
-AND c.ID NOT IN (SELECT Id FROM dbo.vw_OverlappingClipping)
+WITH Base AS (
+	SELECT 
+		c.Id,
+		c.Title,
+		c.Author,
+		c.Type,
+		c.Page,
+		c.StartLocation,
+		c.EndLocation,
+		c.AddedOn,
+		c.Text,
+		c.ImportedAt
+	FROM
+		dbo.Clipping AS c
+	WHERE
+		c.ID NOT IN (SELECT Id FROM dbo.IgnorableClipping)
+	AND c.ID NOT IN (SELECT Id FROM dbo.vw_OverlappingClipping)
 
-UNION ALL
+	UNION ALL
 
-SELECT 
-	o.Id,
-	o.Title,
-	o.Author,
-	o.Type,
-	o.Page,
-	o.StartLocation,
-	o.EndLocation,
-	o.AddedOn,
-	o.Text,
-	o.ImportedAt
-FROM
-	dbo.OldClipping AS o
+	SELECT 
+		o.Id,
+		o.Title,
+		o.Author,
+		o.Type,
+		o.Page,
+		o.StartLocation,
+		o.EndLocation,
+		o.AddedOn,
+		o.Text,
+		o.ImportedAt
+	FROM
+		dbo.OldClipping AS o
+)
+
+SELECT
+	b.Id,
+	COALESCE(s.NewTitle, b.Title) AS Title,
+	b.Author,
+	b.Type,
+	b.Page,
+	b.StartLocation,
+	b.EndLocation,
+	b.AddedOn,
+	b.Text,
+	b.ImportedAt
+FROM 
+	Base AS b
+
+	LEFT JOIN
+	dbo.Substitution AS s
+		ON s.ClippingId = b.Id
